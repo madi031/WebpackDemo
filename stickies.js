c6f7aa28891e6a73e0632f2b7bd49621aea01271
Mathioli Ramalingam <mramali2@ncsu.edu>
2017-07-30 23:01:30 -0400

    demo project

---
+++ stickies.js
@@ -0,0 +1,16 @@
+/*
+module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+			},
+			{
+				use: ['style-loader', 'css-loader'],
+				test: /\.css$/,
+			},
+		],
+    },
+    
+    // loaders are applied right to left, so order is important. css-loader is applied first and its output is sent to style-loader
+*/
